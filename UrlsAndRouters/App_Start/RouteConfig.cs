0b7654335b02075d9f4fef73c402d0e9c877ab40
Kuvuklija <cerakuzz@mail.ru>
2018-12-05 20:47:59 +0300

    ver3

--- UrlsAndRouters/App_Start/RouteConfig.cs
+++ UrlsAndRouters/App_Start/RouteConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,53 +13,56 @@ namespace UrlsAndRouters
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            // IN COMING ROUTES
-            routes.RouteExistingFiles = true;
+            // INCOMING ROUTES
+            routes.RouteExistingFiles = true; //for tern on disk routing throw routing system
 
             routes.MapMvcAttributeRoutes();//turn on atributs routing
                                            //var 1
                                            //Route myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
                                            //routes.Add("MyRoute", myRoute);
 
-            //var 2 first a specific roule - than general
-            //routes.MapRoute("ShopSchema2", "Shop/OldAction", new { controller = "Home", action = "Index" });
-            //routes.MapRoute("ShopSchema", "Shop/{action}", new { controller = "Home" });
-            //routes.MapRoute("MyRoute1", "X{controller}/{action}"); //for variable elements can't use defoults segments
-            //routes.MapRoute("MyRoute2", "Public/{controller}/{action}", new { controller = "Customer", action = "List" }); //statics elements
-            //routes.MapRoute("MyRoute4", "Home/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-            //    new[] { "UrlsAndRouters.Controllers" });
-            //routes.MapRoute("MyRoute5", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-            //    new[] { "UrlsAndRouters.Controllers.AdditionalControllers" });
+      //var 2 first a specific roule - than general
+      //routes.MapRoute("ShopSchema2", "Shop/OldAction", new { controller = "Home", action = "Index" });
+      //routes.MapRoute("ShopSchema", "Shop/{action}", new { controller = "Home" });
+      //routes.MapRoute("MyRoute1", "X{controller}/{action}"); //for variable elements can't use defoults segments
+      //routes.MapRoute("MyRoute2", "Public/{controller}/{action}", new { controller = "Customer", action = "List" }); //statics elements
+      //routes.MapRoute("MyRoute4", "Home/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+      //    new[] { "UrlsAndRouters.Controllers" });
+      //routes.MapRoute("MyRoute5", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+      //    new[] { "UrlsAndRouters.Controllers.AdditionalControllers" });
 
-            //routes.MapRoute("MyRoute3", "{controller}/{action}", new { controller="Home", action="Index"});
+      //routes.MapRoute("MyRoute3", "{controller}/{action}", new { controller="Home", action="Index"});
 
-            //Route myRoute = routes.MapRoute("MyRoute6","{controller}/{action}/{id}/{*catchall}", new{controller="Home",action="Index",id=UrlParameter.Optional},
-            //    new[] {"UrlsAndRouters.Controllers"});
-            //myRoute.DataTokens["UseNamespaceFallback"] = false; //turn of searching in other spaces
+      //Route myRoute = routes.MapRoute("MyRoute6","{controller}/{action}/{id}/{*catchall}", new{controller="Home",action="Index",id=UrlParameter.Optional},
+      //    new[] {"UrlsAndRouters.Controllers"});
+      //myRoute.DataTokens["UseNamespaceFallback"] = false; //turn of searching in other spaces
 
-            //routes.MapRoute("Limit_1", "{controller}/{action}/{id}/{*catchall}",
-            //    new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-            //    new
-            //    {
-            //        controller = "^H.*",
-            //        action = "^Index$|^About$", //prohibit all controllers don't begining on "H"
-            //        httpMethod = new HttpMethodConstraint("GET"),
-            //        //id = new RangeRouteConstraint(10,20)
-            //        id=new CompoundRouteConstraint(new IRouteConstraint[] {
-            //        new AlphaRouteConstraint(),
-            //        new MinLengthRouteConstraint(6)
-            //        })
-            //    },
-            //    new[] { "UrlsAndRouters.Controllers" });
+      //routes.MapRoute("Limit_1", "{controller}/{action}/{id}/{*catchall}",
+      //    new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+      //    new
+      //    {
+      //        controller = "^H.*",
+      //        action = "^Index$|^About$", //prohibit all controllers don't begining on "H"
+      //        httpMethod = new HttpMethodConstraint("GET"),
+      //        //id = new RangeRouteConstraint(10,20)
+      //        id=new CompoundRouteConstraint(new IRouteConstraint[] {
+      //        new AlphaRouteConstraint(),
+      //        new MinLengthRouteConstraint(6)
+      //        })
+      //    },
+      //    new[] { "UrlsAndRouters.Controllers" });
 
-            //routes.MapRoute("ChromeRoute", "{controller}/{action}/{*catchall}",
-            //    new { controller = "Home", action = "Index" },
-            //    new { customConstraint = new UserAgentConstraint("Windows") },
-            //    new[] { "UrlsAndRouters.Controllers.AdditionalControllers" }
-            //);
+      //routes.MapRoute("ChromeRoute", "{controller}/{action}/{*catchall}",
+      //    new { controller = "Home", action = "Index" },
+      //    new { customConstraint = new UserAgentConstraint("Windows") },
+      //    new[] { "UrlsAndRouters.Controllers.AdditionalControllers" }
+      //);
 
-            //OUT COMING ROUTES
-            //routes.MapRoute("MyRoute", "{controller}/{action}");
+      //OUT COMING ROUTES
+      //routes.MapRoute("MyRoute", "{controller}/{action}");
+      //routes.MapRoute("DiskFile", "Content/StaticContent.html", new { controller = "Customer", action = "List" });
+      routes.IgnoreRoute("Content/{filename}.html");
+
             routes.Add(new LegacyRoute("~/articles/Windows_3.1_Overview.html", "~/old/.NET_1.0_Class_Library"));
 
             routes.Add(new Route("SayHello",new CustomRouteHandler()));
--- UrlsAndRouters/Controllers/CustomerController.cs
+++ UrlsAndRouters/Controllers/CustomerController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 namespace UrlsAndRouters.Controllers
 {
     //[RoutePrefix("Global")]
+    [RouteArea("Services")]
     public class CustomerController : Controller
     {
         [Route("~/Test",Name ="AddTest")]
